577fbdf0aa325e5cac551b93a9a9c6a157d622a5
GlennFolker <glenn.folk08@gmail.com>
2023-12-30 18:15:53 +0700

    Outpost

--- src/confictura/content/CPlanets.java
+++ src/confictura/content/CPlanets.java
@@ -1,6 +1,7 @@
 package confictura.content;
 
 import confictura.*;
+import mindustry.content.*;
 import mindustry.type.*;
 
 /**
@@ -8,12 +9,19 @@ import mindustry.type.*;
  * @author GlennFolker
  */
 public final class CPlanets{
+    public static Planet outpost;
+
     private CPlanets(){
         throw new AssertionError();
     }
 
     /** Instantiates all contents. Called in the main thread in {@link ConficturaMod#loadContent()}. */
     public static void load(){
-
+        outpost = new Planet("outpost", Planets.sun, 0.2f, 0){
+            {
+                camRadius = 0.4f;
+                minZoom = 0f;
+            }
+        };
     }
 }
